673829e2797cad97dbd73290abba1a80d8765ac7
ghadeer <ghadeerd@post.bgu.ac.il>
2024-06-23 18:36:45 +0300

    add

---
+++ Backend/BusinessLayer/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IntroSE.Kanban.Backend.ServiceLayer { 
+
+public class User {
+        private string password;
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("email");
+                email = value;
+            }
+        }
+        private string email;
+        public User(string email, string password)
+        { 
+            if(email==null)
+                throw new ArgumentNullException ("email");
+                this.email = email;
+        }
+    }
+}
