b8bfd7306a753cc076a8f8d33832037edb2c10e5
ghadeer <ghadeerd@post.bgu.ac.il>
2024-06-25 17:58:12 +0300

    added user

--- Backend/BusinessLayer/User.cs
+++ Backend/BusinessLayer/User.cs
@@ -1,27 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace IntroSE.kanban2024-2024-100.Backend.BusinessLayer { 
+
+namespace kanban.Backend.Business { 
 
 public class User {
-        private string password;
-        public string Email
-        {
-            get { return email; }
-            set
-            {
-                if (value == null)
-                    throw new ArgumentNullException("email");
-                email = value;
-            }
-        }
         private string email;
-        public User(string email, string password)
-        { 
-            if(email==null)
-                throw new ArgumentNullException ("email");
-                this.email = email;
-        
+        private string password;
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public user(string email, string password, int id)
+        {
+            this.email = email;
+            this.password = password;
+           
+        }
+
+        public string GetEmail() { return email; }
+        public string GetPassword() { return password; }
+        public boolean Login(string password)
+        {
+            if (password.Equals(this.password) == false)return false;
+           
+  
+           
+                return true;
         }
     }
 }
