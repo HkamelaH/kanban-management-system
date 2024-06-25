eb4976d649663a775b1b1695b3d1f2d858622a6b
ghadeer <ghadeerd@post.bgu.ac.il>
2024-06-25 18:00:55 +0300

    added user

--- Backend/ServiceLayer/Response.cs
+++ Backend/ServiceLayer/Response.cs
@@ -1,30 +1,36 @@
 using System;
-using System.Collection.Generic;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 
 
 
 
-namespace IntroSE.Kanban.Backend.ServiceLayer.Response
+namespace kanban.Backend.Service
+
 {
     public class Response
-{
-    public string ErrorMessage { get; set; }
-    public string ReturnValue { get; set; }
-    public Response() { }
-    public Response(string errorMessage)
     {
-        ErrorMessage = errorMessage;
+        public string? ErrorMessage { get; set; }
+        public string? ReturnValue { get; set; }
+        public Response() { }
+        public Response(string errorMessage)
+        {
+            ErrorMessage = errorMessage;
+        }
+        public Response(string errorMessage, string returnValue)
+        {
+            ErrorMessage = errorMessage;
+            ReturnValue = returnValue;
+        }
+       
     }
-    public Response(string errorMessage, string returnValue)
-    {
-        ErrorMessage = errorMessage;
-        ReturnValue = returnValue;
-    }
-    public string ToString()
-    {
 
-    }
-}
+        
+
+
+
 }
 
