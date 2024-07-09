d29740742c721622192608f7913ebb2aec9f2d4c
ghadeer <ghadeerd@post.bgu.ac.il>
2024-07-09 20:45:50 +0300

    comm

--- Backend/DataAccessLayer/DTO/UserDTO.cs
+++ Backend/DataAccessLayer/DTO/UserDTO.cs
@@ -1,8 +1,58 @@
-﻿using System;
+﻿using IntroSE.Kanban.Backend.DataAccessLayer.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Class1
+namespace IntroSE.Kanban.Backend.DataAccessLayer.DTO
 {
-	public Class1()
-	{
-	}
+    public class UserDTO : DTOs
+    {
+
+        public const string UserEmail = "Email";
+        public const string UsersPassword = "Password";
+
+        private string email;
+        private string password;
+        private bool status;
+
+        /// <summary>
+        /// get & set 
+        /// </summary>
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+        public bool Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+        //constructor
+        public UserDTO(string email, string password, bool status) : base(new DALusercontroller())
+        {
+            this.email = email;
+            this.password = password;
+        }
+
+        private static int BoolToInt(bool a)
+        {
+            if (a) return 1;
+            return 0;
+        }
+
+        public void Store()
+        {
+            Email = email;
+            Password = password;
+        }
+    }
 }
