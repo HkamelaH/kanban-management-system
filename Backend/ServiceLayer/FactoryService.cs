dc2951319deff23e5af7c9899738fe461b3d9979
ghadeer <ghadeerd@post.bgu.ac.il>
2024-07-09 13:41:20 +0300

    added

--- Backend/ServiceLayer/FactoryService.cs
+++ Backend/ServiceLayer/FactoryService.cs
@@ -1,8 +1,358 @@
 ﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using IntroSE.Forum.Backend.ServiceLayer;
+using IntroSE.Kanban.Backend.BusinessLayer.User;
+using IntroSE.Kanban.Backend.BussinessLayer.Board;
 
-public class Class1
+namespace IntroSE.Kanban.Backend.ServiceLayer
 {
-	public Class1()
-	{
-	}
+    public class FactoryService
+    {
+        private Boardfacad boardfacad;
+        private UserFacad userFacad;
+        public BoardService boardService;
+        private UserService userService;
+
+        public FactoryService()
+        {
+            userFacad = new UserFacad();
+            boardfacad = new Boardfacad(userFacad);
+            userService = new UserService(userFacad);
+            boardService = new BoardService(boardfacad);
+        }
+
+
+        /// <summary>
+        /// This method registers a new user to the system.
+        /// </summary>
+        /// <param name="email">The user email address, used as the username for logging the system.</param>
+        /// <param name="password">The user password.</param>
+        /// <returns>An empty response, unless an error occurs </returns>
+        public string Register(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        /// <summary>
+        ///  This method logs in an existing user.
+        /// </summary>
+        /// <param name="email">The email address of the user to login</param>
+        /// <param name="password">The password of the user to login</param>
+        /// <returns>A response with the user's email, unless an error occurs</returns>
+        public string Login(string email, string password)
+        {
+            throw new NotImplementedException();
+
+
+        }
+
+        /// <summary>
+        /// This method logs out a logged in user. 
+        /// </summary>
+        /// <param name="email">The email of the user to log out</param>
+        /// <returns>An empty response, unless an error occurs</returns>
+        public string Logout(string email)
+        {
+            throw new NotImplementedException();
+
+
+        }
+        /// <summary>
+        /// This method limits the number of tasks in a specific column.
+        /// </summary>
+        /// <param name="email">The email address of the user, must be logged in</param>
+        /// <param name="boardName">The name of the board</param>
+        /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
+        /// <param name="limit">The new limit value. A value of -1 indicates no limit.</param>
+        /// <returns>An empty response, unless an error occurs</returns>
+        public string LimitTheColumn(string email, string boardName, int columnOrdinal, int limit)
+        {
+            throw new NotImplementedException();
+
+
+        }
+
+        /// <summary>
+        /// This method gets the limit of a specific column.
+        /// </summary>
+        /// <param name="email">The email address of the user, must be logged in</param>
+        /// <param name="boardName">The name of the board</param>
+        /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
+        /// <returns>A response with the column's limit, unless an error occurs</returns>
+        public string GetTheColumnLimit(string email, string boardName, int columnOrdinal)
+        {
+
+            throw new NotImplementedException();
+
+
+        }
+
+
+        /// <summary>
+        /// This method gets the name of a specific column
+        /// </summary>
+        /// <param name="email">The email address of the user, must be logged in</param>
+        /// <param name="boardName">The name of the board</param>
+        /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
+        /// <returns>A response with the column's name, unless an error occurs</returns>
+        public string GetColumnName(string email, string boardName, int columnOrdinal)
+        {
+            throw new NotImplementedException();
+
+        }
+
+
+        /// <summary>
+        /// This method adds a new task.
+        /// </summary>
+        /// <param name="email">Email of the user. The user must be logged in.</param>
+        /// <param name="boardName">The name of the board</param>
+        /// <param name="title">Title of the new task</param>
+        /// <param name="description">Description of the new task</param>
+        /// <param name="dueDate">The due date if the new task</param>
+        /// <returns>An empty response, unless an error occurs</returns>
+        public string AddTask(string email, string boardName, string title, string description, DateTime dueDate)
+        {
+            throw new NotImplementedException();
+
+        }
+
+
+        /// <summary>
+        /// This method updates the due date of a task 
+        /// </summary>
+        /// <param name="email">Email of the user. Must be logged in</param>
+        /// <param name="boardName">The name of the board</param>
+        /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
+        /// <param name="taskId">The task to be updated identified task ID</param>
+        /// <param name="dueDate">The new due date of the column</param>
+        /// <returns>An empty response, unless an error occurs</returns>
+        public string UpdateTaskDateDue(string email, string boardName, int columnOrdinal, int taskId, DateTime dueDate)
+        {
+            throw new NotImplementedException();
+
+        }
+
+
+        /// <summary>
+        /// This method updates task title.
+        /// </summary>
+        /// <param name="email">Email of user. Must be logged in</param>
+        /// <param name="boardName">The name of the board</param>
+        /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
+        /// <param name="taskId">The task to be updated identified task ID</param>
+        /// <param name="title">New title for the task</param>
+        /// <returns>An empty response, unless an error occurs</returns>
+        public string ChangeTaskTitle(string email, string boardName, int columnOrdinal, int taskId, string title)
+        {
+            throw new NotImplementedException();
+
+        }
+
+
+        /// <summary>
+        /// This method updates the description of a task.
+        /// </summary>
+        /// <param name="email">Email of user. Must be logged in</param>
+        /// <param name="boardName">The name of the board</param>
+        /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
+        /// <param name="taskId">The task to be updated identified task ID</param>
+        /// <param name="description">New description for the task</param>
+        /// <returns>An empty response, unless an error occurs</returns>
+        public string ChangeTaskDescription(string email, string boardName, int columnOrdinal, int taskId, string description)
+        {
+            throw new NotImplementedException();
+
+        }
+
+
+        /// <summary>
+        /// This method advances a task to the next column
+        /// </summary>
+        /// <param name="email">Email of user. Must be logged in</param>
+        /// <param name="boardName">The name of the board</param>
+        /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
+        /// <param name="taskId">The task to be updated identified task ID</param>
+        /// <returns>An empty response, unless an error occurs</returns>
+        public string AdvanceTask(string email, string boardName, int columnOrdinal, int taskId)
+        {
+            throw new NotImplementedException();
+
+        }
+
+
+        /// <summary>
+        /// This method returns a column given it's name
+        /// </summary>
+        /// <param name="email">Email of the user, must be logged in</param>
+        /// <param name="boardName">The name of the board</param>
+        /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
+        /// <returns>A response with a list of the column's tasks, unless an error occurs</returns>
+        public string GetColumn(string email, string boardName, int columnOrdinal)
+        {
+            throw new NotImplementedException();
+
+        }
+
+
+
+
+
+        /// <summary>
+        /// This method deletes a board.
+        /// </summary>
+        /// <param name="email">Email of the user, must be logged in and an owner of the board.</param>
+        /// <param name="name">The name of the board</param>
+        /// <returns>An empty response, unless an error occurs</returns>
+        public string DeleteBoard(string email, string name)
+        {
+            throw new NotImplementedException();
+
+        }
+
+
+        /// <summary>
+        /// This method returns all in-progress tasks of a user.
+        /// </summary>
+        /// <param name="email">Email of the user. Must be logged in</param>
+        /// <returns>A response with a list of the in-progress tasks of the user, unless an error occurs</returns>
+        public string ProTasks(string email)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        /// <summary>
+        /// This method returns a list of IDs of all user's boards.
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns>A response with a list of IDs of all user's boards, unless an error occurs</returns>
+        public string GetTheUserBoard(string email)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        /// <summary>
+        /// This method returns a board's name
+        /// </summary>
+        /// <param name="boardId">The board's ID</param>
+        /// <returns>A response with the board's name, unless an error occurs</returns>
+        public string GetTheBoardName(int boardId)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        /// <summary>
+        /// This method adds a user as member to an existing board.
+        /// </summary>
+        /// <param name="email">The email of the user that joins the board. Must be logged in</param>
+        /// <param name="boardID">The board's ID</param>
+        /// <returns>An empty response, unless an error occurs</returns>
+        public string Join(string email, int boardID)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        /// <summary>
+        /// This method removes a user from the members list of a board.
+        /// </summary>
+        /// <param name="email">The email of the user. Must be logged in</param>
+        /// <param name="boardID">The board's ID</param>
+        /// <returns>An empty response, unless an error occurs </returns>
+        public string LeaveTheBoard(string email, int boardID)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        /// <summary>
+        /// This method assigns a task to a user
+        /// </summary>
+        /// <param name="email">Email of the user. Must be logged in</param>
+        /// <param name="boardName">The name of the board</param>
+        /// <param name="columnOrdinal">The column number. The first column is 0, the number increases by 1 for each column</param>
+        /// <param name="taskID">The task to be updated identified a task ID</param>        
+        /// <param name="emailAssignee">Email of the asignee user</param>
+        /// <returns>An empty response, unless an error occurs </returns>
+        public string AssignTheTask(string email, string boardName, int columnOrdinal, int taskID, string emailAssignee)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        ///<summary>This method loads all persisted data.
+        ///<para>
+        ///<b>IMPORTANT:</b> When starting the system via the GradingService - do not load the data automatically, only through this method. 
+        ///In some cases we will call LoadData when the program starts and in other cases we will call DeleteData. Make sure you support both options.
+        ///</para>
+        /// </summary>
+        /// <returns>An empty response, unless an error occurs</returns>
+        public string LoadTheData()
+        {
+            throw new NotImplementedException();
+
+        }
+
+        ///<summary>This method deletes all persisted data.
+        ///<para>
+        ///<b>IMPORTANT:</b> 
+        ///In some cases we will call LoadData when the program starts and in other cases we will call DeleteData. Make sure you support both options.
+        ///</para>
+        /// </summary>
+        ///<returns>An empty response, unless an error occurs</returns>
+
+        public string DeleteTheData()
+        {
+            throw new NotImplementedException();
+
+
+        }
+
+        /// <summary>
+        /// This method transfers a board ownership.
+        /// </summary>
+        /// <param name="currentOwnerEmail">Email of the current owner. Must be logged in</param>
+        /// <param name="newOwnerEmail">Email of the new owner</param>
+        /// <param name="boardName">The name of the board</param>
+        /// <returns>An empty response, unless an error occurs</returns>
+        public string TransferTheOwnership(string currentOwnerEmail, string newOwnerEmail, string boardName)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        /// <summary>
+        /// This method creates a board for the given user.
+        /// </summary>
+        /// <param name="email">Email of the user, must be logged in</param>
+        /// <param name="name">The name of the new board</param>
+        /// <returns>An empty response, unless an error occurs (see <see cref="GradingService"/>)</returns>
+        public string CreateBoard(string email, string name)
+        {
+            throw new NotImplementedException();
+
+        }
+
+
+
+        public string ToJson(object obj)
+        {
+            throw new NotImplementedException();
+
+        }
+
+
+        public bool checkForError(string json)
+        {
+            throw new NotImplementedException();
+
+
+        }
+    }
 }
